72e456999a6c0f6cee5c39311763b6d506fa1f31
annkllye <annkllye@gmail.com>
2023-01-14 15:20:52 +0300

    task3

--- js/lesson1.js
+++ js/lesson1.js
@@ -6,7 +6,7 @@
 // const age = 35;
 // const hoby = "JAVASCRIPT";
 // const message = `ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${hoby}`;
-// console.log(message); 
+// console.log(message);
 
 // ЗАДАЧА 2
 // ЗАПИТАТИ У КОРИСТУВАЧА ІМЯ, ПОЧТУ , ТЕЛЕФОН, І ПОКАЗАТИ У КОНСОЛІ "КОРИСТУВАЧ __ ВИКОРИСТОВУЄ  __ ПОЧТУ І __ ТЕЛЕФОН"
@@ -16,3 +16,12 @@
 // const tel = prompt("Вкажіть Ваш номер телефону");
 // const message = `КОРИСТУВАЧ ${name} ВИКОРИСТОВУЄ  ${mail} ПОЧТУ І ${tel} ТЕЛЕФОН`;
 // console.log(message);
+
+// ЗАДАЧА 3
+// ПОРАХУЙТЕ СУМУ ТОВАРІВ У КОРЗИНІ . ПОТРІБНО ВВЕСТИ СУМУ 3-Х ТОВАРІВ І ПОРАХУВАТИ ЇХ СУМУ
+
+// const price1 = Number(prompt("Вкажіть ціну першого товару"));
+// const price2 = Number(prompt("Вкажіть ціну другого товару"));
+// const price3 = Number(prompt("Вкажіть ціну третього товару"));
+// const totalPrice = price1 + price2 + price3;
+// console.log(totalPrice);
