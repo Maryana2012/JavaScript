2b41a85d9a913f25b2acfc2a3554abe73884b294
IrynaPetr <iryna.bieliakova@gmail.com>
2023-01-14 15:14:05 +0200

    task2

--- js/lesson1.js
+++ js/lesson1.js
@@ -2,8 +2,17 @@
 // РЯДКА "ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ГАЛИНА, МЕНІ 35
 // РОКІВ, Я ВИВЧАЮ JAVASCRIPT"
 
-const name = "Galuna";
-const age = 35;
-const hoby = "JAVASCRIPT";
-const message = `ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${hoby}`;
-console.log(message); 
+// const name = "Galuna";
+// const age = 35;
+// const hoby = "JAVASCRIPT";
+// const message = `ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${hoby}`;
+// console.log(message); 
+
+// ЗАДАЧА 2
+// ЗАПИТАТИ У КОРИСТУВАЧА ІМЯ, ПОЧТУ , ТЕЛЕФОН, І ПОКАЗАТИ У КОНСОЛІ "КОРИСТУВАЧ __ ВИКОРИСТОВУЄ  __ ПОЧТУ І __ ТЕЛЕФОН"
+
+// const name = prompt("Ваше ім'я?");
+// const mail = prompt("Вкажіть Вашу пошту");
+// const tel = prompt("Вкажіть Ваш номер телефону");
+// const message = `КОРИСТУВАЧ ${name} ВИКОРИСТОВУЄ  ${mail} ПОЧТУ І ${tel} ТЕЛЕФОН`;
+// console.log(message);
