0cdebe45bc8f0d710fc0cca2bbe469317199508d
Maryana2012 <maryana.oryshchuk@gmail.com>
2023-01-14 14:57:54 +0200

    1

---
+++ js/lesson1.js
@@ -0,0 +1,9 @@
+ // НАПИШІТЬ РЕЧЕННЯ ЗА ДОПОМОГОЮ ШАБЛОННОГО
+// РЯДКА "ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ГАЛИНА, МЕНІ 35
+// РОКІВ, Я ВИВЧАЮ JAVASCRIPT"
+
+const name = "Galuna";
+const age = 35;
+const hoby = "JAVASCRIPT";
+const message = `ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${hoby}`;
+console.log(message); 
