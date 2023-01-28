a57249bbf6b94a80fa1b96f4bfdd87d5565d258c
Maryana2012 <maryana.oryshchuk@gmail.com>
2023-01-28 14:51:37 +0200

    task1

--- js/lesson1.js
+++ js/lesson1.js
@@ -820,10 +820,10 @@
 
 // автоперевірка 28
 
-const scores = [89, 64, 42, 17, 93, 51, 26];
+// const scores = [89, 64, 42, 17, 93, 51, 26];
 
-const bestScore = Math.max(...scores);
-const worstScore = Math.min(...scores);
+// const bestScore = Math.max(...scores);
+// const worstScore = Math.min(...scores);
 
-console.log(bestScore);
-console.log(worstScore);
+// console.log(bestScore);
+// console.log(worstScore);
