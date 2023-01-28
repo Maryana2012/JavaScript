06166b88e4639679462a4cc0ae552d954c67b94a
Олександр <moskaletzsanych@gmail.com>
2023-01-28 15:08:47 +0200

    task3

--- js/lesson2.js
+++ js/lesson2.js
@@ -34,3 +34,18 @@
 // }
 
 // logItems();
+
+// ЗАДАЧА 3
+// Напишіть код , який шукає саме найменше число в масиві
+// 1) напишіть циклом for
+// const numbers = [2, 17, 94, 1, 23, 37];
+// 2) перепишіть функцією findSmallestNumber(numbers)
+// // console.log(findSmallestNumber([2, 17, 94, 1, 23, 37]));
+// // console.log(findSmallestNumber([49, 4, 83, 7, 12]));
+
+// const numbers = [2, 17, 94, 1, 23, 37];
+// let smallestNumber = 0;
+
+// for (const number of numbers) {
+//   console.log(number);
+// }
