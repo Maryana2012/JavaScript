f22e8ca1c672c6e9b25a6d8856e4dccd65bafcf5
Maryana2012 <maryana.oryshchuk@gmail.com>
2023-02-11 15:08:36 +0200

    task2

--- js/lesson3.js
+++ js/lesson3.js
@@ -3,13 +3,36 @@
 // letMeSeeYourName(callback) - запитує імя користувача , через prompt і викликає колбек функцію (callback)
 // greet(name) - колбек , який приймає імя і логінує в консоль рядок  "Привіт" + імя
 
-function letMeSeeYourName(callback) {
-  const name = promt("Введіть ваше імʼя");
-  callback(name);
-}
+// function letMeSeeYourName(callback) {
+//   const name = prompt("Введіть ваше імʼя");
+//   callback(name);
+// }
 
-function greet(name) {
-  console.log(`Привіт ${name}`);
-}
+// function greet(name) {
+//   // console.log(`Привіт ${name}`);
+//   alert(name);
+// }
 
-letMeSeeYourName(greet);
+// letMeSeeYourName(greet);
+
+
+
+// ЗАДАЧА 2
+// Напишіть дві функції:
+// makeProduct(name, price, callback) -приймає імя та ціну товару, а також колбек.Функція створює обєкт товару, добавляє йому унікальний ідентифікатор, в значення id і викликає колбек, передаючи йому створений обєкт.
+// showProduct(product) - колбек приймає обєкт продукта і логінує його в консоль;
+// makeProduct("Холодильник", 10000, logger);
+
+// const makeProduct = (name, price, callback) => {
+//   const product = {
+//     name,
+//     price,
+//     id: Date.now()
+//   }
+//   callback(product);
+// }
+
+// const showProduct = (product) => {
+// console.log(product);
+// }
+// makeProduct("Холодильник", 10000, showProduct);
