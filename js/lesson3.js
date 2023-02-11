5c384d0888ac0d81c98397fdf9e756708d5dd0ee
Oleksandr-Kharkiv <115045030+Oleksandr-Kharkiv@users.noreply.github.com>
2023-02-11 14:22:21 +0100

    3

--- js/lesson3.js
+++ js/lesson3.js
@@ -36,3 +36,107 @@
 // console.log(product);
 // }
 // makeProduct("Холодильник", 10000, showProduct);
+
+
+// ЗАДАЧА 3
+// МАЄМО МАСИВ, ПОТРІБНО:
+// 
+// 2)порахувати загальну суму років скільки прожили вчені, 
+// і визначити середнє значення;
+// 3) відсортувати по алфавіту;
+// 4)відсортувати по кількості прожитих років;
+// 5)відфільтрувати, хто народився в 15,16,17ст;
+// 6)знайти рік народження Albert Einstein;
+// 7)визначити, чи усі вчені працювали у 19ст;
+const scientists = [
+    {
+      name: "Albert",
+      surname: "Einstein",
+      born: 1879,
+      dead: 1955,
+      id: 1,
+    },
+    {
+      name: "Isaac",
+      surname: "Newton",
+      born: 1643,
+      dead: 1727,
+      id: 2,
+    },
+    {
+      name: "Galileo",
+      surname: "Galilei",
+      born: 1564,
+      dead: 1642,
+      id: 3,
+    },
+    {
+      name: "Marie",
+      surname: "Curie",
+      born: 1867,
+      dead: 1934,
+      id: 4,
+    },
+    {
+      name: "Johannes",
+      surname: "Kepler",
+      born: 1571,
+      dead: 1630,
+      id: 5,
+    },
+    {
+      name: "Nicolaus",
+      surname: "Copernicus",
+      born: 1473,
+      dead: 1543,
+      id: 6,
+    },
+    {
+      name: "Max",
+      surname: "Planck",
+      born: 1858,
+      dead: 1947,
+      id: 7,
+    },
+    {
+      name: "Katherine",
+      surname: "Blodgett",
+      born: 1898,
+      dead: 1979,
+      id: 8,
+    },
+    {
+      name: "Ada",
+      surname: "Lovelace",
+      born: 1815,
+      dead: 1852,
+      id: 9,
+    },
+  
+    {
+      name: "Lise",
+      surname: "Meitner",
+      born: 1878,
+      dead: 1968,
+      id: 11,
+    },
+    {
+      name: "Sarah E.",
+      surname: "Goode",
+      born: 1855,
+      dead: 1905,
+      id: 10,
+    },
+    {
+      name: "Hanna",
+      surname: "Hammarström",
+      born: 1829,
+      dead: 1909,
+      id: 12,
+    },
+  ];
+//   1)получити масив вчених які народились у 19ст;
+
+let myScientist = scientists.filter((scientist) => 
+scientist.born >= 1800 && scientist.born < 1900).map((scientist) => `${scientist.name} ${scientist.surname}`)
+console.log(myScientist);
