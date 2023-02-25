7339820506f01aa9547b70e7d69564e24147c854
IrynaPetr <iryna.bieliakova@gmail.com>
2023-02-25 15:37:54 +0200

    task 2

--- js/lesson4.js
+++ js/lesson4.js
@@ -33,3 +33,31 @@
 
 
 // console.log(cardCar);
+
+// ЗАДАЧА 2
+// 2.1НАПИШІТЬ КАЛЬКУЛЯТОР, ЯКИЙ БУДЕ РАХУВАТИ ЦІНУ ШАШЛИКА, В ЗАЛЕЖНОСТІ ВІД ЦІНИ ЗА 
+// КІЛОГРАМ І КІЛЬКОСТІ 
+// КУПЛЕНОГО ПРОДУКТА , І БУДЕ ВИВОДИТИ РЕЗУЛЬТАТ НА ЕКРАН
+
+
+const refs = {
+  priceEl: document.querySelector('#price'),
+  quantityEl: document.querySelector('#quantity'),
+  totalEl: document.querySelector('.total'),
+  formEl:document.querySelector('.form'),
+  amountEl: document.querySelector('.amount'),
+
+}
+// console.log(refs);
+
+function calc() {
+  const price = refs.priceEl.value;
+  const quantity = refs.quantityEl.value;
+ const totalPrice = price * quantity;
+refs.totalEl.textContent = totalPrice;
+refs.amountEl.textContent = quantity;
+
+}
+calc();
+window.addEventListener('DOMContentLoaded', calc);
+refs.formEl.addEventListener('input', calc);
