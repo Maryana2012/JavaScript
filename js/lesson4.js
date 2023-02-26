df43f2a86b0969c42265c746bb7a177493cde9ca
Maryana2012 <maryana.oryshchuk@gmail.com>
2023-02-26 11:12:26 +0200

    додаткове завданя

--- js/lesson4.js
+++ js/lesson4.js
@@ -1,4 +1,4 @@
-// ЗАДАЧА 1 
+// ЗАДАЧА 1
 // Створіть карточки товару з масиву використовуючи createElement
 
 // import cars from "./data/cars.js";
@@ -35,8 +35,8 @@
 // console.log(cardCar);
 
 // ЗАДАЧА 2
-// 2.1НАПИШІТЬ КАЛЬКУЛЯТОР, ЯКИЙ БУДЕ РАХУВАТИ ЦІНУ ШАШЛИКА, В ЗАЛЕЖНОСТІ ВІД ЦІНИ ЗА 
-// КІЛОГРАМ І КІЛЬКОСТІ 
+// 2.1НАПИШІТЬ КАЛЬКУЛЯТОР, ЯКИЙ БУДЕ РАХУВАТИ ЦІНУ ШАШЛИКА, В ЗАЛЕЖНОСТІ ВІД ЦІНИ ЗА
+// КІЛОГРАМ І КІЛЬКОСТІ
 // КУПЛЕНОГО ПРОДУКТА , І БУДЕ ВИВОДИТИ РЕЗУЛЬТАТ НА ЕКРАН
 
 
@@ -92,50 +92,112 @@
 // ЗАДАЧА 3
 // 3.1 Є МАСИВ, ПОТРІБНО СТВОРИТИ РОЗМІТКУ ІЗ ТИХ КАРТОК через <ul class="js-list"></ul>
 
-const instruments = [{
-  id: 1,
-  img: "https://content.rozetka.com.ua/goods/images/big/13664457.jpg",
-  name: "Молоток",
-  price: 150
+// const instruments = [{
+//   id: 1,
+//   img: "https://content.rozetka.com.ua/goods/images/big/13664457.jpg",
+//   name: "Молоток",
+//   price: 150
+// },
+// {
+//   id: 2,
+//   img: "https://machtz.com.ua/files/resized/products/dsc_2898_new.800x550.jpg",
+//   name: "Перфоратор",
+//   price: 3000
+// },{
+//   id: 3,
+//   img: "https://content2.rozetka.com.ua/goods/images/big_tile/232653359.jpg",
+//   name: "Рівень",
+//   price: 2000
+// }]
+
+// const instrumentsEl = document.querySelector('.js-list');
+ 
+// function makeCard() {
+// const acc = instruments.map(({id, img, name, price}) => {
+//   return `<li data-id = "${id}">
+//   <h2>${name}</h2>
+//   <img src="${img}" alt="${name}" width="150">
+//   <button type="submit" class="buy">Купити</button>
+//   <p>${price}</p>
+//   </li>`
+// }).join(' ')
+// instrumentsEl.insertAdjacentHTML("beforeend", acc);
+// }
+// makeCard();
+
+// // 3.2 ДОБАВТЕ КНОПКУ, ЧЕРЕЗ ЯКУ МИ КУПЛЯТИМЕМО ТОВАР
+
+// function makeButton(e) {
+//   if(!e.target.classList.contains("buy")){
+//     return
+//   }
+//   const idEl = e.target.closest("li");
+//   // console.log(idEl);
+//   const setEl = idEl.dataset.id;
+//   // console.log(setEl);
+//   const findEl = instruments.find(instrument => instrument.id === Number(setEl))
+//   console.log(findEl);
+// }
+// instrumentsEl.addEventListener("click", makeButton);
+
+// ---------------------------------------------------
+Додакове завдання на домашнє опрацювання
+
+ЗАДАЧА 4
+ПОТРІБНО СТВОРИТИ Ф-ЦІЮ , ЯКА БУДЕ ШУКАТИ НАРЦИСА.ГОЛОВНА УМОВА НАРЦИСА ЗНАЮТЬ УСІ , НАРЦИС НЕ ЗНАЄ НІКОГО
+// є елемент масиву , який незнає нікого
+// якщо не одна така людина , то вертаємо not found
+// якщо один , то перевірити , чи його знають усі люди, якщо так то вертаємо found
+
+const people1 = [{
+  name: 'Alex',
+  know: ["Eva", "Jhon"],
 },
 {
-  id: 2,
-  img: "https://machtz.com.ua/files/resized/products/dsc_2898_new.800x550.jpg",
-  name: "Перфоратор",
-  price: 3000
-},{
-  id: 3,
-  img: "https://content2.rozetka.com.ua/goods/images/big_tile/232653359.jpg",
-  name: "Рівень",
-  price: 2000
+  name: 'Ivan',
+  know: ["Jhon", "Alex"],
+},
+{
+  name: 'Eva',
+  know: ["Alex", "Jhon"],
+},
+{
+  name: 'Jhon',
+  know: [],
 }]
 
-const instrumentsEl = document.querySelector('.js-list');
- 
-function makeCard() {
-const acc = instruments.map(({id, img, name, price}) => {
-  return `<li data-id = "${id}">
-  <h2>${name}</h2> 
-  <img src="${img}" alt="${name}" width="150">
-  <button type="submit" class="buy">Купити</button>
-  <p>${price}</p>
-  </li>`
-}).join(' ')
-instrumentsEl.insertAdjacentHTML("beforeend", acc);
-}
-makeCard();
 
-// 3.2 ДОБАВТЕ КНОПКУ, ЧЕРЕЗ ЯКУ МИ КУПЛЯТИМЕМО ТОВАР
+const people2 = [{
+  name: 'Alex',
+  know: ["Eva", "Jhon"],
+},
+{
+  name: 'Jhon',
+  know: ["Eva"],
+},
+{
+  name: 'Eva',
+  know: [],
+},
+{
+  name: 'Ivan',
+  know: ["Jhon", "Alex"],
+}]
 
-function makeButton(e) {
-  if(!e.target.classList.contains("buy")){
-    return
-  }
-  const idEl = e.target.closest("li");
-  // console.log(idEl);
-  const setEl = idEl.dataset.id;
-  // console.log(setEl);
-  const findEl = instruments.find(instrument => instrument.id === Number(setEl)) 
-  console.log(findEl);
-}
-instrumentsEl.addEventListener("click", makeButton);
+
+const people3 = [{
+  name: 'Alex',
+  know: ["Eva", "Jhon"],
+},
+{
+  name: 'Jhon',
+  know: [],
+},
+{
+  name: 'Eva',
+  know: ["Alex", "Jhon"],
+},
+{
+  name: 'Ivan',
+  know: ["Jhon", "Alex"],
+}]
