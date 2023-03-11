73e6cd709e387437d6bf73b1e717b5c9e55c1e6c
Mariia <79396737+MariiaMelychyn@users.noreply.github.com>
2023-03-11 15:12:08 +0200

    Update lesson5.js

--- js/lesson5.js
+++ js/lesson5.js
@@ -6,16 +6,34 @@
 // якщо скрол більше висоту хедера, в іншому випадку його 
 // видаляємо
 
-const headerEl = document.querySelector('.header');
-// console.log(headerEl.clientHeight)
-// console.log(headerEl.clientWidth)
-const navEl = document.querySelector('.nav');
-// console.log(navEl)
-const headerHeightEl = headerEl.clientHeight;
-pageYOffset;
-function check (){
-    if(pageYOffset < headerHeightEl) {
-        navEl.classList.add('fixed-nav');
-    } navEl.classList.remove('fixed-nav');
-}
-window.addEventListener('scroll', check);
+// const header = document.querySelector(".header");
+// // console.log(headerEl.clientHeight);
+// // console.log(headerEl.clientWidth)
+// const nav = document.querySelector(".nav");
+// // console.log(navEl)
+// const headerHeight = header.clientHeight;
+// pageYOffset;
+// function check (){
+//     if(pageYOffset > headerHeight) {
+//         nav.classList.add('fixed-nav');
+//     } nav.classList.remove('fixed-nav');
+// }
+// window.addEventListener('scroll', check);
+
+// 2 варіант
+
+// const header = document.querySelector(".header");
+// const nav = document.querySelector(".nav");
+
+// const headerHeight = header.clientHeight;
+// pageYOffset;
+
+// function navbarFixed() {
+//   if (pageYOffset > headerHeight) {
+//     nav.classList.add("fixed-nav");
+//   } else {
+//     nav.classList.remove("fixed-nav");
+//   }
+// }
+
+// window.addEventListener("scroll", navbarFixed);
